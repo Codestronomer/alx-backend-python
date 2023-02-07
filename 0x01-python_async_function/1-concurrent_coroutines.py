78752b90acee51320e9b130b972da00c85ea5aa6
codestronomer <johnrumide6@gmail.com>
2023-02-07 02:06:51 +0100

    [fix] change coroutine logic

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -23,6 +23,5 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     return:
         (List): A list of floats returned by every call to wait_random
     """
-    delay_list = await asyncio.gather(*[wait_random(max_delay)
-    for _ in range(n)])
-    return sorted(delay_list)
+    delay_tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(delay_tasks)]
